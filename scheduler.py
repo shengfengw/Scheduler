f31dc9c43db8671aa7b294677c1fc57c15f7a91f
Shengfeng Wang <shengfeng@kei.attlocal.net>
2020-09-30 11:36:23 -0700

    added comments

--- scheduler.py
+++ scheduler.py
@@ -4,67 +4,66 @@ from scipy.sparse.linalg import LinearOperator
 from scipy.optimize import LinearConstraint
 from scipy.optimize import Bounds
 
-#input
-a = np.array([[0,1,1,1,1,1,0,0],
-                [1,1,1,1,0,0,0,0],
-                [0,1,1,1,0,0,0,0],
-                [0,0,1,1,1,1,1,0],
-                [0,0,1,1,1,1,1,1],
-                [0,0,0,1,1,1,0,0]])
+# input
+# a is the msg sending window
+a = np.array([[0,1,1,1,1,1,0,0,0,0],
+                [1,1,1,1,0,0,0,0,0,0],
+                [0,1,1,1,0,0,0,0,0,0],
+                [0,0,1,1,1,1,1,0,0,0],
+                [0,0,1,1,1,1,1,1,1,1],
+                [0,1,1,1,1,1,1,1,0,0],
+                [0,1,1,1,1,1,1,1,1,1],
+                [0,0,0,1,1,1,1,1,0,0]])
 
-m = np.array([400,500,400,450,600,300])
+# m is the number of msgs to be sent
+m = np.array([40000,50000,40000,45000,60000,80000,70000,30000])
 
-r=len(a[:,1])
-c=len(a[1,:])
+r = len(a[:,1])
+c = len(a[1,:])
 
+# constraint is sum of msgs sent in intervals equals number of msgs to be sent
 def constructConstraint(a,m):
-    aVectorArr=[]
+    aVectorArr = []
     for i in range(r):
-        aVector=np.zeros(r*c)
+        aVector = np.zeros(r*c)
         for j in range(c):
-            aVector[i*c+j]=a[i][j]
+            aVector[i*c+j] = a[i][j]
         aVectorArr.extend(aVector)
     aVectorArr = np.reshape(aVectorArr, (r, r*c))
     linear_constraint = LinearConstraint(aVectorArr,m,m)
-    print(aVectorArr)
     return linear_constraint
 
+# bound of msg sent in interval is [0,0] or [0,m[i]]
 def constructBound(a,m):
-    r=len(a[:,1])
-    c=len(a[1,:])
-    minArr=np.zeros(r*c)
-    maxArr=[]
+    r = len(a[:,1])
+    c = len(a[1,:])
+    minArr = np.zeros(r*c)
+    maxArr = []
     for i in range(r):
         for j in range(c):
             maxArr.append(m[i]*a[i][j])
-    print(maxArr)
     return Bounds(minArr,maxArr)
-
-linear_constraint=constructConstraint(a,m)
-bounds = constructBound(a,m)
-x0 = a
-
 
 def objFn(x):
     x = np.reshape(x, (r, c))
-    cSumArr=[]
-    for j in range(c):
-        cSum=0
-        for i in range(r):
-            cSum+=a[i][j]*x[i][j]
-        cSumArr.append(cSum)
-    res=0
-    # sum of square of diff in column sum
+    cSumArr = np.sum(x,axis=0)
+    res = 0
+    # sum of square of diff in column sum, better performance than max(cSumArr)
     for i in range(c):
         for j in range(i+1,c):
-            res+=(cSumArr[i]-cSumArr[j])**2
+            res += (cSumArr[i] - cSumArr[j])**2
     return res
 
+linear_constraint = constructConstraint(a,m)
+bounds = constructBound(a,m)
+x0 = a
 x0 = x0.flatten()
 res = minimize(objFn, x0, method='trust-constr',
                 constraints=[linear_constraint],
                 options={'maxiter': 500, 'verbose': 1},bounds=bounds)
-#print(res)
-res=np.rint(res.x)
-res=np.reshape(res,(r,c))
+
+res = np.reshape(res.x,(r,c))
+res = np.rint(res)
 print(res)
+cSum = np.sum(res,axis=0)
+print(cSum)
